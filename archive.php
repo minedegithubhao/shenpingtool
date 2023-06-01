97450737b03f2c6979e0002c65884f73514d4d80
minedegithubhao <824737880@qq.com>
2023-06-02 06:31:55 +0800

    调整浏览次数也阅读次数的位置

--- archive.php
+++ archive.php
@@ -1,11 +1,14 @@
 <?php get_header() ?>
-
 	<div class="article">
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
 		<div class="entry">
 			<h2 class="entry-title"><a href="<?php the_permalink()?>" title="<?php the_title()?>"><?php the_title()?></a></h2>
 			<div class="entry-meta">
+                <span>
+                    <em class="kan"><?php echo getPostViews(get_the_ID())?></em>
+                    <em class="ping"><?php comments_popup_link('0','1','%') ?></em>
+                </span>
 				<?php the_category( ',')?> | <?php the_time( 'Y年m月d日 H:i:s' )?>
 				<?php edit_post_link('编辑本文')?>
 			</div><!-- .entry-meta -->
@@ -23,10 +26,7 @@
 				</div><!-- .entry-arch -->
 			</div><!-- .entry-cnt -->
 			<div class="entry-tag">
-<!--                <span>-->
-                    <span><?php echo getPostViews(get_the_ID())?>浏览</span>
-					<span><?php comments_popup_link('0','1','%') ?>次评论</span>
-<!--				</span>-->
+                <a href="<?php echo get_comments_link()?>">添加评论</a>
 				<a class="more" href="<?php the_permalink()?>" >查看全文»</a>
 <!--				标签：-->
 <!--				--><?php //if( has_tag() ) :  ?>
--- index.php
+++ index.php
@@ -5,7 +5,10 @@
 		<div class="entry">
 			<h2 class="entry-title"><a href="<?php the_permalink()?>" title="<?php the_title()?>"><?php the_title()?></a></h2>
 			<div class="entry-meta">
-
+                <span>
+                    <em class="kan"><?php echo getPostViews(get_the_ID())?></em>
+                    <em class="ping"><?php comments_popup_link('0','1','%') ?></em>
+                </span>
 				<?php the_category( ',')?> | <?php the_time( 'Y年m月d日 H:i:s' )?> 
 				<?php edit_post_link('编辑本文')?>
 			</div><!-- .entry-meta -->
@@ -24,8 +27,7 @@
 				</div><!-- .entry-arch -->
 			</div>
 			<div class="entry-tag">
-                <span><?php echo getPostViews(get_the_ID())?>浏览</span>
-                <span><?php comments_popup_link('0','1','%') ?>次评论</span>
+                <a href="<?php echo get_comments_link()?>">添加评论</a>
 				<a class="more" href="<?php the_permalink()?>" >查看全文»</a>
 			</div>
 		</div><!-- .entry -->			
